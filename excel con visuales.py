6ea4436367f99276e6e65a97565f82ef950c0440
LuchoLatella <lucho_latella@hotmail.com>
2024-08-02 12:08:24 -0300

    agregado posibilidad de elegir tipo de gruadado

--- excel con visuales.py
+++ excel con visuales.py
@@ -115,10 +115,26 @@ class App:
     
     def descargar_archivo(self):
         if self.data is not None:
-            file_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("Archivos CSV", "*.csv")])
+            file_path = filedialog.asksaveasfilename(
+                defaultextension=".csv",
+                filetypes=[
+                    ("CSV (Delimitado por comas)", "*.csv"),
+                    ("Excel Workbook", "*.xlsx"),
+                    ("Excel 97-2003 Workbook", "*.xls"),
+                    ("Excel con macros habilitados", "*.xlsm"),
+                ]
+            )
             if file_path:
                 try:
-                    self.data.to_csv(file_path, index=False, encoding='ISO-8859-1', sep=';')
+                    if file_path.endswith(".csv"):
+                        self.data.to_csv(file_path, index=False, encoding='ISO-8859-1', sep=';')
+                    elif file_path.endswith(".xlsx"):
+                        self.data.to_excel(file_path, index=False, engine='openpyxl')
+                    elif file_path.endswith(".xls"):
+                        self.data.to_excel(file_path, index=False)
+                    elif file_path.endswith(".xlsm"):
+                        self.data.to_excel(file_path, index=False, engine='openpyxl')
+                    
                     messagebox.showinfo("Información", f"Archivo guardado exitosamente en: {file_path}")
                 except Exception as e:
                     messagebox.showerror("Error", f"No se pudo guardar el archivo: {str(e)}")
@@ -129,3 +145,4 @@ class App:
 root = tk.Tk()
 app = App(root)
 root.mainloop()
+
