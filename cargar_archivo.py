cd398dcf82e72f06e72386033f2c845b804add1f
LuchoLatella <lucho_latella@hotmail.com>
2024-08-04 16:44:16 -0300

    creada  el módulo de cargar archivo .csv

---
+++ cargar_archivo.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from tkinter import filedialog, messagebox
+
+def cargar_archivo_csv(app):
+    file_path = filedialog.askopenfilename(title="Selecciona un archivo CSV", filetypes=[("Archivos CSV", "*.csv")])
+    if file_path:
+        try:
+            app.data = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+            app.text_area.insert(tk.END, "Archivo CSV cargado exitosamente.\n")
+            app.text_area.insert(tk.END, app.data.head().to_string() + "\n")
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo cargar el archivo CSV: {str(e)}")
+
+def cargar_archivo_excel(app):
+    file_path = filedialog.askopenfilename(title="Selecciona un archivo Excel", filetypes=[("Archivos Excel", "*.xls *.xlsx")])
+    if file_path:
+        try:
+            app.data = pd.read_excel(file_path)
+            app.text_area.insert(tk.END, "Archivo Excel cargado exitosamente.\n")
+            app.text_area.insert(tk.END, app.data.head().to_string() + "\n")
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo cargar el archivo Excel: {str(e)}")
